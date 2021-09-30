19d378a3ad46f6b1335f029d49e565f32e806730
nukeanag <rkddmsdk0125@naver.com>
2021-09-30 18:53:26 +0900

    Update week3_homework.py

--- week3_homework.py
+++ week3_homework.py
@@ -8,7 +8,7 @@ data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200
 soup = BeautifulSoup(data.text, 'html.parser')
 
 trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
-# 아래 빈 칸('')을 채워보세요
+
 for tr in trs:
     rank = tr.select_one('td.number').text[0:2].strip()
     title = tr.select_one('td.info > a.title.ellipsis').text.strip()
